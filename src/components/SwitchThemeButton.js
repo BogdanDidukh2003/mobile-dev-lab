078605719d3c296079771786d001bc62d1a9dfed
Denys Shutka <nden2105@gmail.com>
2020-11-14 16:13:14 +0200

    fix: format code

--- src/components/SwitchThemeButton.js
+++ src/components/SwitchThemeButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Button, TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 
-import CONSTANTS from './../constants';
 import Styles from './../styles';
 import { ThemeContext } from './../util';
 
@@ -9,7 +8,6 @@ const SwitchThemeButton = () => {
   const { theme, switchTheme } = React.useContext(ThemeContext);
 
   return (
-    <>
     <TouchableOpacity
       onPress={() => {
         switchTheme();
@@ -19,18 +17,6 @@ const SwitchThemeButton = () => {
         style={[Styles[theme].textStyle, Styles[theme].switchThemeButton]}
       >&#10227;</Text>
     </TouchableOpacity>
-    {/*<Button
-      title='&#10227;'
-      onPress={() => {
-        switchTheme();
-      }}
-      color={
-        theme == CONSTANTS.THEMES.LIGHT
-          ? CONSTANTS.COLORS.SKY_BLUE
-          : CONSTANTS.COLORS.LIGHT_ORANGE
-      }
-    />*/}
-    </>
   );
 };
 
